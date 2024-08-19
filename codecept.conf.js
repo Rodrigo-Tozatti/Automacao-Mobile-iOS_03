6ee52b07311a6d1782f382ec83a83266f8ee119b
Rodrigo Rosseti Tozatti <rodrigo.tozatti@hotmail.com>
2024-08-18 21:21:18 -0300

    Instalações e configurações funcionando

---
+++ codecept.conf.js
@@ -0,0 +1,33 @@
+const {
+  setHeadlessWhen,
+  setCommonPlugins
+} = require('@codeceptjs/configure');
+
+setHeadlessWhen(process.env.HEADLESS);
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  name: 'automation_appium',
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Appium: {
+      appiumV2: true,
+      url: 'http://127.0.0.1:4723/', // URL do servidor Appium
+      app: '/Users/rodrigorossetitozatti/Repositorio-Mac/Automacao-Mobile-iOS/app/qazandoapp.app',
+      desiredCapabilities: {
+        platformName: 'iOS',
+        platformVersion: '16.2', // Versão do sistema oper
+        deviceName: 'iPhone 14 Pro Max', // Nome do dispositivo
+        automationName: 'XcUiTest',
+      },
+      path: '/'
+    }
+  },
+  include: {
+    I: './steps_file.js', // Local do arquivo de etapas
+
+    loginPage: "./pages/login_page.js"
+  },
+};
